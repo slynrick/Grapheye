e9ef5beb761052de6ff5e853262b0813e94bdce4
Matheus Brito Ribeiro <matheustrkk@gmail.com>
2019-05-16 15:34:14 -0300

    colocando funcionalidades do menu lateral para funcionar

--- public/src/app/graph/graph.component.ts
+++ public/src/app/graph/graph.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GraphService } from './graph.service';
 import { graphFromBack } from './graph-from-back';
 import * as shape from 'd3-shape';
-import { Graph, Node } from '@swimlane/ngx-graph';
+import { Graph, Node, Layout } from '@swimlane/ngx-graph';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -16,10 +16,44 @@ export class GraphComponent implements OnInit {
 
 	graphsFromBack: graphFromBack;
 	graphs: Graph = {nodes: [], edges: []};
-	curve = shape.curveLinear;
-
+	
 	center$: Subject<boolean> = new Subject();
 	update$: Subject<boolean> = new Subject();
+
+	draggingEnabled = true;
+	enableZoom = true;
+	panningEnabled = true;
+
+	curveType: string = 'Bundle';
+	curve: any = shape.curveLinear;
+	interpolations = [
+		'Bundle',
+		'Cardinal',
+		'Catmull Rom',
+		'Linear',
+		'Monotone X',
+		'Monotone Y',
+		'Natural',
+		'Step',
+		'Step After',
+		'Step Before'
+	]; 
+
+	layout: String | Layout = 'dagre';
+	layouts: any[] = [
+	  {
+		label: 'Dagre',
+		value: 'dagre',
+	  },
+	  {
+		label: 'Cola Force Directed',
+		value: 'colaForceDirected'
+	  },
+	  {
+		label: 'D3 Force Directed',
+		value: 'd3ForceDirected',
+	  },
+	];
 
 	constructor(private graphService: GraphService) { }
 
@@ -32,10 +66,6 @@ export class GraphComponent implements OnInit {
 		this.graphs.nodes.push(node);
 		this.updateGraph();
 		this.id = '';
-	}
-
-	setId(id: string){
-		this.id = id;
 	}
 
 	centerGraph() {
@@ -55,19 +85,70 @@ export class GraphComponent implements OnInit {
 	}
 	
 	buildGraph() {
-			this.graphs.nodes = this.graphsFromBack.vertices.map((vertice)=>{
-				return {
-					id: vertice,
-					label: vertice
-				}
-				
-			})
-			this.graphs.edges = this.graphsFromBack.arestas.map((aresta)=>{
-				return {
-					source: aresta[0],
-					target: aresta[1]
-				}
-			})
+		this.graphs.nodes = this.graphsFromBack.vertices.map((vertice)=>{
+			return {
+				id: vertice,
+				label: vertice
+			}
+			
+		})
+		this.graphs.edges = this.graphsFromBack.arestas.map((aresta)=>{
+			return {
+				source: aresta[0],
+				target: aresta[1]
+			}
+		})
+	}
+
+	toggleDraggable(){
+		this.draggingEnabled = !this.draggingEnabled;
+	}
+
+	toggleZoom(){
+		this.enableZoom = !this.enableZoom;
+	}
+
+	togglePanning(){
+		this.panningEnabled = !this.panningEnabled;
+	}
+
+	setLayout(layoutName: string): void {
+		this.layout = layoutName;
+		this.centerGraph();
+	}
+
+	setInterpolationType(curveType) {
+	this.curveType = curveType;
+	if (curveType === 'Bundle') {
+		this.curve = shape.curveBundle.beta(1);
+	}
+	if (curveType === 'Cardinal') {
+		this.curve = shape.curveCardinal;
+	}
+	if (curveType === 'Catmull Rom') {
+		this.curve = shape.curveCatmullRom;
+	}
+	if (curveType === 'Linear') {
+		this.curve = shape.curveLinear;
+	}
+	if (curveType === 'Monotone X') {
+		this.curve = shape.curveMonotoneX;
+	}
+	if (curveType === 'Monotone Y') {
+		this.curve = shape.curveMonotoneY;
+	}
+	if (curveType === 'Natural') {
+		this.curve = shape.curveNatural;
+	}
+	if (curveType === 'Step') {
+		this.curve = shape.curveStep;
+	}
+	if (curveType === 'Step After') {
+		this.curve = shape.curveStepAfter;
+	}
+	if (curveType === 'Step Before') {
+		this.curve = shape.curveStepBefore;
+	}
 	}
 
 }
